3aca9a689616e2cacce94709ad7a01052edc0ffa
HarisFaris17 <harisfaris17@gmail.com>
2022-08-31 06:20:21 +0700

    Convenient threshold and adaptive threshold

--- Threshold.py
+++ Threshold.py
@@ -7,14 +7,31 @@ cv.imshow('Original',frame)
 gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
 cv.imshow('Gray',gray)
 
-ret, thresholdedFrame = cv.threshold(gray,90,255,cv.THRESH_BINARY)
-cv.imshow('Threshold',thresholdedFrame)
-print(thresholdedFrame)
+# input threshold must be grayscaled
+# function threshold will convert all pixels of grayscaled image that have intensities (binary represented) greater than thresh and less than maxVal to maxVal. And convert all pixels that have intensities less than tresh and greater than maxVal to 0.
+ret, thresholdedGray = cv.threshold(gray,90,255,cv.THRESH_BINARY)
+cv.imshow('Threshold',thresholdedGray)
+print(thresholdedGray)
+
+# cv.THRESH_BINARY_INV will inverse the above thresholdedGray
+retInv, thresholdedGrayInv = cv.threshold(gray,90,255,cv.THRESH_BINARY_INV)
+cv.imshow('Threshold Inverse',thresholdedGrayInv)
+
+# Adaptive thresholding simply find the best thresh value
+AdaptiveThreshold = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,11,3)
+print('Adaptive')
+print(AdaptiveThreshold)
+ThreeChannelsAdaptiveThreshold = cv.merge([AdaptiveThreshold,AdaptiveThreshold,AdaptiveThreshold])
+print(ThreeChannelsAdaptiveThreshold)
+cv.imshow('Adaptive Thresholding',AdaptiveThreshold)
+
+ThresholdedFrameByAdaptiveThreshold = cv.bitwise_and(frame,ThreeChannelsAdaptiveThreshold)
+cv.imshow('Haa',ThresholdedFrameByAdaptiveThreshold)
 
 histGray = cv.calcHist([gray],[0],None,[256],[0,256])
 
 # apply masking to gray with mask image is thresholdFrame
-maskedGray = cv.bitwise_and(gray,thresholdedFrame)
+maskedGray = cv.bitwise_and(gray,thresholdedGray)
 cv.imshow('Masked Gray',maskedGray)
 
 histMaskedGray = cv.calcHist([maskedGray],[0],None,[256],[0,256])
@@ -27,6 +44,7 @@ plt.xlabel('Binary representation of pixels')
 plt.ylabel('# of pixels')
 
 plt.figure(2)
+# since the pixels that have 0 intensity is much more than pixels that have other intensities, therefore we shouldn't included it so that the histogram can be seen clearly of other intensities
 plt.plot(histMaskedGray[1:])
 plt.title('Histogram Masked')
 plt.xlabel('Binary representation of pixels')
