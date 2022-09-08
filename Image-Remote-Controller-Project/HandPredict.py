e702757a4f1374d8117f55ed0ff1802ca466f4de
HarisFaris17 <harisfaris17@gmail.com>
2022-09-08 17:24:29 +0700

    load the model

--- Image-Remote-Controller-Project/HandPredict.py
+++ Image-Remote-Controller-Project/HandPredict.py
@@ -1,6 +1,6 @@
 import pickle
 import cv2 as cv
-import 
+from HandTraining import trainingDirectory,modelHandDirectory
 from sklearn.ensemble import RandomForestClassifier
 
 cap = cv.VideoCapture(0)
@@ -8,6 +8,6 @@ cap.set(cv.CAP_PROP_FRAME_WIDTH,1280)
 cap.set(cv.CAP_PROP_FRAME_WIDTH,720)
 
 # Load the model
-# classifier = pickle.load()
+classifier = pickle.load(open(trainingDirectory+modelHandDirectory,'rb'))
 
 # while True:
